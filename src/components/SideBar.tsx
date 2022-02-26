3b1d4b745ce2a783e2f45b01e39d60479c38732b
Waldener Monteiro <waldener.dev@gmail.com>
2022-02-26 17:02:54 -0300

    refactor: separating components and finishing challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { MouseEventHandler } from "react";
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+type SideBarProps = {
+  genres: GenreProps[];
+  onSelectGenre: (genre: GenreProps) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({ genres, onSelectGenre, selectedGenreId }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button key={String(genre.id)} title={genre.title} iconName={genre.name} onClick={() => onSelectGenre(genre)} selected={selectedGenreId === genre.id} />
+        ))}
+      </div>
+    </nav>
+  );
 }
